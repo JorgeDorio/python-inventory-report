b59d051d39baf917a83832de2cdb03872da0817f
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-11-03 16:31:25 -0300

    2 - Testar o relatório individual do produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,21 +1,26 @@
 from inventory_report.inventory.product import Product
+from .mocks import MockProduct
 
-mockProduct = {
-        "id": 1,
-        "nome_do_produto":"Buds 3",
-        "nome_da_empresa": "xiaomi",
-        "data_de_fabricacao": "11/03/2022",
-        "data_de_validade": "11/03/2023",
-        "numero_de_serie": "1234560",
-        "instrucoes_de_armazenamento": "Limpar semanalmente"
-        }
 
 def test_cria_produto():
-    product = Product(mockProduct["id"], mockProduct["nome_do_produto"], mockProduct["nome_da_empresa"], mockProduct["data_de_fabricacao"], mockProduct["data_de_validade"], mockProduct["numero_de_serie"], mockProduct["instrucoes_de_armazenamento"])
-    assert product.id == mockProduct["id"]
-    assert product.nome_do_produto == mockProduct["nome_do_produto"]
-    assert product.nome_da_empresa == mockProduct["nome_da_empresa"]
-    assert product.data_de_fabricacao == mockProduct["data_de_fabricacao"]
-    assert product.data_de_validade == mockProduct["data_de_validade"]
-    assert product.numero_de_serie == mockProduct["numero_de_serie"]
-    assert product.instrucoes_de_armazenamento == mockProduct["instrucoes_de_armazenamento"]
+    mock = MockProduct()
+    product = Product(
+        mock.product["id"],
+        mock.product["nome_do_produto"],
+        mock.product["nome_da_empresa"],
+        mock.product["data_de_fabricacao"],
+        mock.product["data_de_validade"],
+        mock.product["numero_de_serie"],
+        mock.product["instrucoes_de_armazenamento"],
+    )
+
+    assert product.id == mock.product["id"]
+    assert product.nome_do_produto == mock.product["nome_do_produto"]
+    assert product.nome_da_empresa == mock.product["nome_da_empresa"]
+    assert product.data_de_fabricacao == mock.product["data_de_fabricacao"]
+    assert product.data_de_validade == mock.product["data_de_validade"]
+    assert product.numero_de_serie == mock.product["numero_de_serie"]
+    assert (
+        product.instrucoes_de_armazenamento
+        == mock.product["instrucoes_de_armazenamento"]
+    )
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,25 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from ..product.mocks import MockProduct
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mock = MockProduct()
+    product = Product(
+        mock.product["id"],
+        mock.product["nome_do_produto"],
+        mock.product["nome_da_empresa"],
+        mock.product["data_de_fabricacao"],
+        mock.product["data_de_validade"],
+        mock.product["numero_de_serie"],
+        mock.product["instrucoes_de_armazenamento"],
+    )
+
+    data = product.__repr__()
+
+    assert data == (
+        f"O produto {product.nome_do_produto} "
+        f"fabricado em {product.data_de_fabricacao} "
+        f"por {product.nome_da_empresa} com validade "
+        f"até {product.data_de_validade} "
+        f"precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
